33b129407cb5d6a0e1d7bcd589f06bb9d2569bf8
AlucardDH <damienhembert@gmail.com>
2015-11-26 09:54:25 +0100

    [YT Video Hide] v 0.4 Prise en compte du nouveau layout grille

--- yt_hide_videos.user.js
+++ yt_hide_videos.user.js
@@ -1,16 +1,13 @@
 // ==UserScript==
 // @name			DH - Youtube hide video
 // @namespace		https://github.com/AlucardDH/userscripts
-// @version			0.3.1
+// @version			0.4
 // @author			AlucardDH
 // @projectPage		https://github.com/AlucardDH/userscripts
 // @downloadURL		https://github.com/AlucardDH/userscripts/raw/master/yt_hide_videos.user.js
 // @updateURL		https://github.com/AlucardDH/userscripts/raw/master/yt_hide_videos.user.js
-// @match			https://www.youtube.com
-// @match			https://www.youtube.com*
-// @include        	https://www.youtube.com
-// @include        	https://www.youtube.com*
-// @require 		http://code.jquery.com/jquery-2.1.4.min.js
+// @match        	https://www.youtube.com/feed/subscriptions*
+// @require 	 	https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
 // @grant			GM_addStyle
 // @grant			GM_getValue
 // @grant			GM_setValue
@@ -20,91 +17,48 @@
 // ==/UserScript==
 
 console.log("DH - Youtube hide video : loaded !");
-var youtube_item = ".yt-shelf-grid-item>div";
-var youtube_item2 = ".feed-item-dismissable";
-
-function clickOnFollowButton (jNode) {
-    var clickEvent  = document.createEvent ('MouseEvents');
-    clickEvent.initEvent ('click', true, true);
-    jNode[0].dispatchEvent (clickEvent);
+unsafeWindow.exportHidden = function() {
+    var result = "";
+    
+	var keys = GM_listValues();
+    for (var i=0,key=null; key=keys[i]; i++) {
+        if(result.length>0) result += ",";
+        result += key;
+    }
+	console.log(result);
 }
 
-function isHidden(videoId) {
-	var value = GM_getValue(videoId);
-	if(value!==null) {
-		return true;
-	}
+unsafeWindow.importHidden = function(idsString) {
+	var keys = idsString.split(",");
+    for (var i=0,key=null; key=keys[i]; i++) {
+        hide(key);
+    }
 }
 
-function hide(videoId) {
-	GM_setValue(videoId,true);
+function isHidden(itemId) {
+    return GM_getValue(itemId);
 }
 
-function addHideButton(element,videoId) {
-	if(element.attr("data-hide-button-done")) {
-		return;
-	}
-	
-	element.attr("data-hide-button-done","true");
-	var button = $("<button>Cacher</button>");
-	button.click(function() {
-		hide(videoId);
-		element.parent().remove();
-	});
-	element.append(button);	
+function hide(itemId) {
+    GM_setValue(itemId,true);
 }
 
-function addHideButton2(element,videoId) {
-	if(element.attr("data-hide-button-done")) {
-		return;
-	}
-	
-	element.attr("data-hide-button-done","true");
-	
-	var button = $("<button>Cacher</button>");
-	button.click(function() {
-		hide(videoId);
-		
-		clickOnFollowButton(element.find("button.flip"));//.click();
-		setTimeout(function(){
-			clickOnFollowButton(element.find(".dismiss-menu-choice"));//.click();
-		//	video.parent().parent().parent().parent().remove();
-		},100);
-		
-	});
-	element.append(button);	
+function hideWatched() {
+    $(".watched-badge").closest('.yt-shelf-grid-item').remove();
+    $.each($(".yt-lockup"),function(index,element) {
+        var e = $(element);
+        var itemId = e.attr("data-context-item-id");
+        if(isHidden(itemId)) {
+            e.closest('.yt-shelf-grid-item').remove();
+        } else {
+            if(!e.hasClass("dhdone")) {
+                var a = $('<a>Cacher</a>');
+                a.click(function(){hide(itemId);});
+                e.append(a);
+                e.addClass("dhdone");
+            }
+        }
+    });
 }
 
-
-
-var interval = setInterval(function() {
-	var videos = $(youtube_item);
-	//console.log(videos);
-	$.each(videos,function(index,video) {
-		video = $(video);
-		var videoId = video.attr("data-context-item-id");
-		if(isHidden(videoId)) {
-			video.parent().remove();
-		} else {
-			addHideButton(video,videoId);
-		}
-	});
-	var videos = $(youtube_item2);
-	//console.log(videos);
-	$.each(videos,function(index,video) {
-		video = $(video);
-		var videoId = $(video.find(".yt-lockup")).attr("data-context-item-id");
-		if(isHidden(videoId)) {
-			console.log(videoId+" should be hidden");
-			
-			clickOnFollowButton(video.find("button.flip"));//.click();
-			setTimeout(function(){
-				clickOnFollowButton(video.find(".dismiss-menu-choice"));//.click();
-			//	video.parent().parent().parent().parent().remove();
-			},100);
-			
-		} else {
-			addHideButton2(video,videoId);
-		}
-	});
-},5000);
+setInterval(hideWatched,1000);
