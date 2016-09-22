42bf389a5055aad3605c9b8a875f714103c7801c
damienhembert <damien.hembert@gfi.fr>
2016-09-22 10:00:13 +0200

    Jira style : color added

--- old jira_style.user.js
+++ old jira_style.user.js
@@ -1,22 +1,26 @@
 // ==UserScript==
-// @name         DH - Old Jira+
+// @name         DH - Jira+
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @match        https://jira.gfi.fr/secure/IssueNavigator.jspa*
-// @require 	 https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js
+// @match        https://delivery.gfi.fr/jira/issues*
 // @grant        none
 // ==/UserScript==
 
 var color = {
  //   'Affectée':'#FFE74C',
-    'En test':'#FFE97F',
-    'A livrer en recette':'#CCFFCC',
-    'En recette':'#AAFFAA',
-    'Fermée':'#C0C0C0'
+    'en test':'#FFE97F',
+    'in test':'#FFE97F',
+    'completed':'#CCFFCC',
+    'a livrer en recette':'#CCFFCC',
+    'en recette':'#AAFFAA',
+    'a livrer en prod':'#AAFFAA',
+    'en prod':'#AAFFAA',
+    'fermée':'#C0C0C0',
+    'closed':'#C0C0C0'
 };
 
-console.log("Jira+ loaded");
-
+/*
 (function() {
     $(".issuerow").each(function(index,line) {
         line = $(line);
@@ -31,3 +35,18 @@ console.log("Jira+ loaded");
     });
 
 })(); 
+*/
+
+
+console.log("Jira+ loaded");
+var issues = document.getElementsByClassName("issuerow");
+for(var i=0;i<issues.length;i++) {
+    var status = issues[i].getElementsByClassName("status")[0].innerText.trim().toLowerCase();
+    var c = color[status];
+    if(c) {
+        issues[i].style.background=c;
+    }
+}
+
+document.getElementsByClassName("focused")[0].classList.remove("focused");
+
