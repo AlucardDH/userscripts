596097b0d8a3c51468fefcb05fb70b96c7c6c1f0
damienhembert <damien.hembert@gfi.fr>
2017-04-21 13:34:34 +0200

    - hangout gif : +9gag

--- hangout_gif.user.js
+++ hangout_gif.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Google mail GIF auto
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  Replace llink to gif by the image
+// @version      0.2
+// @description  Replace link to gif or 9gag by the image
 // @author       You
-// @match        https://hangouts.google.com/webchat*
+// @match        https://hangouts.google.com/webchat/u/0/frame*
 // @grant        none
 // ==/UserScript==
 
@@ -27,6 +27,24 @@ function apply() {
             var img = document.createElement("img");
             img.src = link.innerText;
             img.style = "max-width:100%";
+            img.addEventListener("click", function(){
+                window.open(link.innerText, "_blank");
+            });
+            replace(img,link);
+        }
+        
+        // 9gag
+        // https://9gag.com/gag/aPBr4Vw
+        // to : https://img-9gag-fun.9cache.com/photo/aPBr4Vw_700b.jpg
+        if(link.innerText.indexOf("9gag.com/gag/")>-1) {
+            console.log("loading 9gag",link.innerText,link);
+            var imgLink = "https://img-9gag-fun.9cache.com/photo"+link.innerText.substring(link.innerText.lastIndexOf("/"))+"_700b.jpg";
+            var img = document.createElement("img");
+            img.src = imgLink;
+            img.style = "max-width:100%";
+            img.addEventListener("click", function(){
+                window.open(link.innerText, "_blank");
+            });
             replace(img,link);
         }
 
@@ -36,5 +54,5 @@ function apply() {
 (function() {
     'use strict';
     
-    setInterval(apply,5000);
+    setInterval(apply,2500);
 })();
