0dbab254e68cc487441cecd358444f21c86fc32a
damienhembert <damien.hembert@gfi.fr>
2017-06-06 11:42:17 +0200

    [guide vocal 0.2] - fix : date n'apparaissant plus - add : affichage auto de la validité quand click

--- orange_vocal_guide.user.js
+++ orange_vocal_guide.user.js
@@ -1,14 +1,38 @@
 // ==UserScript==
 // @name         DH - Orange Guide Vocal Modernize
-// @version      0.1
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  try to take over the world!
 // @author       Damien Hembert
-// @match        https://www.guidevocal.fr.orange-business.com/vocalmestre2/service.jsf
+// @match        https://www.guidevocal.fr.orange-business.com/vocalmestre*/service.jsf
 // @require 	 https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
 // ==/UserScript==
 
 var popup = $(".editPanelPopup");
 function isPopupVisible() {
     return popup.css("visibility")=="hidden";
+}
+
+function initAutoShowValidite() {
+    $.each($(".rf-trn-lbl:contains(mobile)"),function(index,user) {
+        if(!user.onclick) {
+            user.onclick = showValidite;
+        }
+    });
+}
+
+function showValidite() {
+    if(isValidityVisible()) {
+        setTimeout(showValidite,1000);
+        return;
+    }
+
+    var tab = $('[id="editPanelTabForm:button3"]');
+    if(tab && tab.length>0) {
+        tab.click();
+    } else {
+        setTimeout(showValidite,1000);
+    }
 }
 
 var timers = {};
@@ -20,11 +44,11 @@ function changeIfDifferent(field,newValue,max) {
     if(max && newValue>max) return;
     var currentValue = field.find("option[selected]").val();
     if(currentValue==newValue) return;
-    
+
     console.log("Changing "+currentValue+" to "+newValue);
-    
+
     var timerId = field[0].id.replace(/.*:/,"");
-    
+
     if(timers[timerId]=="locked") {
         console.log(timerId,"is locked");
         setTimeout(function(){
@@ -39,7 +63,7 @@ function changeIfDifferent(field,newValue,max) {
     if(currentValue!=newValue) {
         field.find("option").removeAttr('selected');
         field.find("option[value="+newValue+"]").attr('selected',"selected");
-            
+
         timers[timerId] = setTimeout(function() {
             console.log(timerId,"running");
             field.trigger("change");
@@ -68,22 +92,23 @@ var dates = [];
 
 function updateDate() {
     if(isValidityVisible()) {
-        var selected = $("img[src='/vocalmestre2/static/images/tree/transfer_tr_sel.gif']").closest(".rf-tr-nd").index()-1;
-        
+        var selected = $("img[src*='transfer_tr_sel.gif']").closest(".rf-tr-nd").index()-1;
+
         var startDateDay = $("select[id*='beginDays']");
         var startDateMonth = $("select[id*='beginMonth']");
         var startDateYear = $("select[id*='beginYear']");
-        
+
         var endDateDay = $("select[id*='endDays']");
         var endDateMonth = $("select[id*='endMonth']");
         var endDateYear = $("select[id*='endYear']");
-        
+
         var text = startDateDay.val()+"/"+startDateMonth.val()+"/"+startDateYear.val()+" - "+endDateDay.val()+"/"+endDateMonth.val()+"/"+endDateYear.val();
         dates[selected] = text;
     }
-    
+
     $.each($(".rf-trn-lbl:contains(mobile)"),function(index,user) {
         user = $(user);
+
         if(dates[index]) {
             var date = user.next();
             if(date.length===0) {
@@ -97,16 +122,16 @@ function updateDate() {
 
 function validityModernize() {
     var forcedYear = 2017;
-    
+
     if(isValidityVisible() && !isValidityModernized()) {
         var trs = $("#validityForm tr");
         $(trs[0]).append($('<td><span class="componentLabel">date</span></td>'));
-        
+
         // Start date
         var startDateDay = $("select[id*='beginDays']");
         var startDateMonth = $("select[id*='beginMonth']");
         var startDateYear = $("select[id*='beginYear']");
-        
+
         var startDateField = $('<input type="date" value="'+(forcedYear && startDateYear.val()<forcedYear ? forcedYear : startDateYear.val())+'-'+startDateMonth.val()+'-'+startDateDay.val()+'"/>');
         startDateField.change(function(){
             var v = $(this).val();
@@ -116,17 +141,17 @@ function validityModernize() {
                 changeIfDifferent($("select[id*='beginMonth']"),date[1],12);
                 changeIfDifferent($("select[id*='beginDays']"),date[2],31);
             }
-            
+
         });
         var startDateFieldTd = $('<td></td>');
         startDateFieldTd.append(startDateField);
         $(trs[1]).append(startDateFieldTd);
-        
+
         // End date
         var endDateDay = $("select[id*='endDays']");
         var endDateMonth = $("select[id*='endMonth']");
         var endDateYear = $("select[id*='endYear']");
-        
+
         var endDateField = $('<input type="date" value="'+(forcedYear && endDateYear.val()<forcedYear ? forcedYear : endDateYear.val())+'-'+endDateMonth.val()+'-'+endDateDay.val()+'"/>');
         endDateField.change(function(){
             var v = $(this).val();
@@ -145,3 +170,4 @@ function validityModernize() {
 
 setInterval(validityModernize,1000);
 setInterval(updateDate,1000);
+setInterval(initAutoShowValidite,1000);
