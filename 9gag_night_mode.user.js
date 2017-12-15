5bb529e2cca133f6f8b2f36bcb435f2956889fee
damienhembert <damien.hembert@gfi.fr>
2017-12-15 10:25:43 +0100

    - add : 9gag night mode (forgotten file) - add : outlook365 (new ui) deletall search

---
+++ 9gag_night_mode.user.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name			DH - 9gag night mode
+// @namespace		https://github.com/AlucardDH/userscripts
+// @version			0.1
+// @author			AlucardDH
+// @projectPage		https://github.com/AlucardDH/userscripts
+// @match        	https://9gag.com/*
+// @require 	 	https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
+// @grant		 GM_addStyle
+// ==/UserScript==
+
+console.log("DH - 9gag night mode : loaded !");
+
+function styleToString(style) {
+	var result = style.selector;
+	result += "{";
+	for(var property in style) {
+		if(property.indexOf("elector")<0) {
+			result += property+":"+style[property]+";";
+		}
+
+	}
+	result += "}";
+
+	return result;
+}
+
+GM_addStyle(styleToString({selector:"body","background":"black url('http://colourlovers.com.s3.amazonaws.com/images/patterns/2/2875.png?1197341146') repeat scroll center center"}));
+GM_addStyle(styleToString({selector:"div","background":"transparent !important","color":"white"}));
+GM_addStyle(styleToString({selector:"a","color":"white !important"}));
+GM_addStyle(styleToString({selector:"h1","color":"white !important"}));
+GM_addStyle(styleToString({selector:".section-sidebar","background":"black !important"}));
+GM_addStyle(styleToString({selector:".section-sidebar *","color":"white !important"}));
+GM_addStyle(styleToString({selector:".share","display":"none"}));
+GM_addStyle(styleToString({selector:"a.btn","background":"black !important","color":"white"}));
+GM_addStyle(styleToString({selector:".notification-list li *","background":"black !important"}));
+
+$("#sidebar-content").remove();
---
+++ outlook_new_delete_search.user.js
@@ -0,0 +1,60 @@
+// ==UserScript==
+// @name         Outlook delete all search
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  try to take over the world!
+// @author       You
+// @match        https://outlook.office.com/owa/*
+// @match        https://outlook.office365.com/owa/*
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
+// @grant           GM_getValue
+// @grant           GM_setValue
+// @grant           GM_deleteValue
+// @grant           unsafeWindow
+// ==/UserScript==
+
+function selectAndDelete() {
+    $("[title='Sélectionner tous les éléments dans la vue']").click();
+    $("[title='Supprimer (Suppr)']").click();
+}
+
+function getSearch() {
+    return $("[aria-label*='Effectuer des recherches dans les messages']").val();
+}
+
+function somethingToDelete() {
+    console.log('checkboxes',$("[role=checkbox]").length);
+    return $("[role=checkbox]").length>1;
+}
+
+unsafeWindow.deleteAllSearch = function() {
+  if(getSearch() && somethingToDelete()) {
+      GM_setValue('deleteAll',getSearch());
+      waitingForReload = 10;
+  }
+};
+
+waitingForReload = 0;
+(function() {
+    'use strict';
+
+    //var supprBtn = $("[title='Supprimer (Suppr)']");
+    var container = $(".o365cs-nav-centerAlign");
+
+    var deleteAll = $('<button class="_fce_i ms-fwt-r ms-fcl-np o365button">Tous</button>');
+    deleteAll.click(deleteAllSearch);
+
+
+    setInterval(function(){
+        if(GM_getValue('deleteAll') && getSearch()==GM_getValue('deleteAll') && !somethingToDelete() && waitingForReload>0) {
+            $("._is_w.o365button").click();
+            waitingForReload--;
+        } else if(GM_getValue('deleteAll') && getSearch()==GM_getValue('deleteAll') && somethingToDelete()) {
+            selectAndDelete();
+            waitingForReload = 10;
+        } else {
+            GM_deleteValue('deleteAll');
+        }
+    },1000);
+
+})();
