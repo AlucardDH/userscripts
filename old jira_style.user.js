d8132f550c72071de69607cd25a406eb9d5a34a0
dhembert <damien.hembert@gfi.fr>
2018-07-03 11:36:09 +0200

    Jira+ : - add : new status

--- old jira_style.user.js
+++ old jira_style.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DH - Jira+
 // @namespace    http://tampermonkey.net/
-// @version      0.2.1
+// @version      0.2.3
 // @match        https://jira.gfi.fr/secure/IssueNavigator.jspa*
 // @match        https://delivery.gfi.fr/jira/*
 // @grant        none
@@ -9,14 +9,18 @@
 
 var color = {
  //   'Affectée':'#FFE74C',
+    'work in progress':'#FFE97F',
     'en test':'#FFE97F',
     'in test':'#FFE97F',
+    'test':'#FFE97F',
     'completed':'#CCFFCC',
+    'finished':'#CCFFCC',
     'a livrer en recette':'#CCFFCC',
     'a livrer recette':'#CCFFCC',
     'en recette':'#AAFFAA',
     'livré recette':'#AAFFAA',
     'a livrer en prod':'#AAFFAA',
+    'ok pour mep':'#AAFFAA',
     'en prod':'#AAFFAA',
     'fermée':'#C0C0C0',
     'closed':'#C0C0C0'
@@ -42,15 +46,15 @@ var color = {
 
 console.log("Jira+ loaded");
 setInterval(function(){
-	var issues = document.getElementsByClassName("issuerow");
-	for(var i=0;i<issues.length;i++) {
-	    var status = issues[i].getElementsByClassName("status")[0].innerText.trim().toLowerCase();
-	    var c = color[status];
-	    if(c) {
-	        issues[i].style.background=c;
-	    }
-	}
+var issues = document.getElementsByClassName("issuerow");
+for(var i=0;i<issues.length;i++) {
+    var status = issues[i].getElementsByClassName("status")[0].innerText.trim().toLowerCase();
+    var c = color[status];
+    if(c) {
+        issues[i].style.background=c;
+    }
+}
 
-	document.getElementsByClassName("focused")[0].classList.remove("focused");
+document.getElementsByClassName("focused")[0].classList.remove("focused");
 },5000);
 
