8763bf1c77716b120f20fac4e18bd17ed71e5bb2
dhembert <damien.hembert@gfi.fr>
2019-03-07 15:20:16 +0100

    9gag : hide voted - add : console hideVideos() and showVideos()

--- 9gag_hide_rated.user.js
+++ 9gag_hide_rated.user.js
@@ -1,21 +1,68 @@
 // ==UserScript==
 // @name			DH - 9gag hide voted
 // @namespace		https://github.com/AlucardDH/userscripts
-// @version			0.2
+// @version			0.3
 // @author			AlucardDH
 // @projectPage		https://github.com/AlucardDH/userscripts
-// @match        	http://9gag.com/*
-// @exclude        	http://9gag.com/gag*
-// @exclude         http://9gag.com/u/alucarddh/*
+// @match        	https://9gag.com/*
+// @exclude        	https://9gag.com/gag*
+// @exclude         https://9gag.com/u/alucarddh/*
 // @require 	 	https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
+// @grant           GM_getValue
+// @grant           GM_setValue
 // ==/UserScript==
 
 console.log("DH - 9gag hide voted : loaded !");
+
+// ----------------- USERSCRIPT UTILS -----------------
+
+var SCRIPT_BASE = "9GAG";
+
+function setScriptParam(key,value) {
+    GM_setValue(SCRIPT_BASE+"_"+key,value);
+}
+
+function getScriptParam(key) {
+    return GM_getValue(SCRIPT_BASE+"_"+key);
+}
+
+function hasScriptParam(key) {
+    return getScriptParam(key);
+}
+
+
+
+// ------------- VIDEO -------------
+var SHOW_VIDEOS = 'showVideos';
+function showVideo() {
+    var value = getScriptParam(SHOW_VIDEOS);
+    if(value===false) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+unsafeWindow.showVideos = function() {
+    if(!showVideo()) {
+        setScriptParam(SHOW_VIDEOS,true);
+        document.location.reload();
+    }
+};
+
+unsafeWindow.hideVideos = function() {
+    if(showVideo()) {
+        setScriptParam(SHOW_VIDEOS,false);
+    }
+};
 
 function hideVoted() {
     $(".active").closest('article').remove();
     if($(".badge-entry-collection").text().trim()=="") {
         $(".badge-entry-collection").css("height","100px");
+    }
+    if(!showVideo()) {
+        $("video").closest('article').remove();
     }
 }
 
