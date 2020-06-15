48f7ea1946988b0d0ab410799a158d8fd2983547
Damien Hembert <damienhembert@gmail.com>
2020-06-15 12:01:35 +0200

    Auto click "accept" or "agree" button in a cookie message

---
+++ auto_accept_cookies.user.js
@@ -0,0 +1,59 @@
+// ==UserScript==
+// @name         DH - Auto accept cookies
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Auto click "accept" or "agree" button in a cookie message
+// @author       Damien Hembert
+// @match        *://*/*
+// @require 	 https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
+// @grant           GM_getValue
+// @grant           GM_setValue
+// ==/UserScript==
+
+// ----------------- USERSCRIPT UTILS -----------------
+
+var SCRIPT_BASE = "CookiesAccept";
+
+function setScriptParam(key,value) {
+    GM_setValue(SCRIPT_BASE+"_"+key,value);
+}
+
+function getScriptParam(key) {
+    return GM_getValue(SCRIPT_BASE+"_"+key);
+}
+
+function hasScriptParam(key) {
+    return getScriptParam(key);
+}
+
+var TAG = ['a','button'];
+var LABEL = ['accept','agree'];
+
+function buildButtonSelector() {
+    var result = '';
+    for(var i=0;i<TAG.length;i++) {
+        for(var j=0;j<LABEL.length;j++) {
+            if(result) {
+                result +=',';
+            }
+            result += TAG[i]+':contains("'+LABEL[j]+'")';
+        }
+    }
+    return result;
+}
+
+(function() {
+    'use strict';
+    if(!hasScriptParam(document.location.hostname)) {
+
+        setTimeout(function() {
+            var button = $(':contains("cookie")').find(buildButtonSelector());
+            if(button.length>0) {
+                console.log(button);
+                setScriptParam(document.location.hostname,"true");
+                button.click();
+            }
+        },1000);
+    }
+    //
+})();
