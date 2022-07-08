a35fe4b0cf616e01d26ea7139546e0787d3dfc20
AlucardDH <damienhembert@gmail.com>
2022-07-08 15:28:43 +0200

    YTH Lite 0.3.0 : support for Polymer shadow Dom

--- yt_hide_videos_light.user.js
+++ yt_hide_videos_light.user.js
@@ -1,22 +1,20 @@
 // ==UserScript==
 // @name            DH - Youtube hide video LIGHT
 // @namespace       https://github.com/AlucardDH/userscripts
-// @version         0.2.0
+// @version         0.3.0
 // @author          AlucardDH
 // @projectPage     https://github.com/AlucardDH/userscripts
 // @downloadURL     https://raw.githubusercontent.com/AlucardDH/userscripts/master/yt_hide_videos_light.user.js
 // @updateURL       https://raw.githubusercontent.com/AlucardDH/userscripts/master/yt_hide_videos_light.user.js
 // @match           https://www.youtube.com/*
 // @exclude         https://www.youtube.com/watch*
-// @require         https://zeptojs.com/zepto.min.js
+// @require         https://cdnjs.cloudflare.com/ajax/libs/cash/8.1.1/cash.min.js
 // @grant           GM_getValue
 // @grant           GM_setValue
 // @grant           GM_xmlhttpRequest
 // @grant           GM_addStyle
 // @grant           unsafeWindow
 // ==/UserScript==
-
-unsafeWindow.jquery=$;
 
 function styleToString(style) {
     var result = style.selector;
@@ -39,8 +37,17 @@ var DISPLAY_SHORTS = false;
 var previousWatchedCount = 0;
 var previousShortsCount = 0;
 
+var VIDEO_TAGS = ['ytd-grid-video-renderer'.toUpperCase(),'ytd-video-renderer'.toUpperCase(),'ytd-rich-item-renderer'.toUpperCase()];
+function getVideoElement(e) {
+    var current = e;
+    while(e && VIDEO_TAGS.indexOf(e.tagName)==-1) {
+        e = e.__shady_native_parentElement;
+    }
+    return e;
+}
+
 function displayWatched(b) {
-    var watched = $('ytd-thumbnail-overlay-resume-playback-renderer').closest('ytd-grid-video-renderer,ytd-video-renderer,ytd-rich-item-renderer ');
+    var watched = $('ytd-thumbnail-overlay-resume-playback-renderer').map((i,e)=>getVideoElement(e));
     var count = watched.length;
     if(b!=DISPLAY_WATCHED || previousWatchedCount!=count) {
         DISPLAY_WATCHED = b;
@@ -55,7 +62,7 @@ function displayWatched(b) {
 }
 
 function displayShorts(b) {
-    var shorts = $('ytd-thumbnail-overlay-time-status-renderer[overlay-style=SHORTS]').closest('ytd-grid-video-renderer,ytd-video-renderer,ytd-rich-item-renderer ');
+    var shorts = $('ytd-thumbnail-overlay-time-status-renderer[overlay-style=SHORTS]').map((i,e)=>getVideoElement(e));
     var count = shorts.length;
     if(b!=DISPLAY_SHORTS || previousShortsCount!=count) {
         DISPLAY_SHORTS = b;
@@ -96,16 +103,16 @@ function addButtons() {
           +'</div>'
         +'</tp-yt-paper-toggle-button>'
     );
-    toggleButton.click(toggleWatched);
+    toggleButton.on('click',toggleWatched);
     toggleGroup.append(toggleButton);
 
-    var toggleButton = $(
+    toggleButton = $(
         '<tp-yt-paper-toggle-button id="toggle" style="vertical-align:middle;display: inline-block;" touch-action: pan-y; noink="" class="style-scope ytd-settings-switch-renderer" role="button" toggles="" active="" checked="">'
            +'<div class="toggle-label style-scope tp-yt-paper-toggle-button" style="display:inline-block;height:24px;padding:8px;line-height:24px;">HIDE SHORTS'
           +'</div>'
         +'</tp-yt-paper-toggle-button>'
     );
-    toggleButton.click(toggleShorts);
+    toggleButton.on('click',toggleShorts);
     toggleGroup.append(toggleButton);
 
     var pos = $('ytd-masthead #end');
@@ -119,5 +126,5 @@ $(function() {
     setInterval(update,1000);
 });
 
-console.log("DH - Youtube hide video LIGHT 0.2.0 : loaded !");
+console.log("DH - Youtube hide video LIGHT 0.3.0 : loaded !");
 
